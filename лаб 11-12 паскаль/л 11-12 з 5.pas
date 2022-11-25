57af620291e1b6db089e3d69e40a454e0486ab5b
Екатерина Созина <118255097+KatyaKatyaKatyaSSS@users.noreply.github.com>
2022-11-26 01:14:45 +0300

    Add files via upload

---
+++ лаб 11-12 паскаль/л 11-12 з 5.pas
@@ -0,0 +1,21 @@
+﻿Program naconecto;
+uses graphabc;
+BEGIN
+setpencolor(clblack);
+circle(10,50,10);
+FloodFill(10,50,clBlue);
+circle(50,80,20);
+FloodFill(50,80,clPurple);
+circle(100,120,30);
+FloodFill(100,120,clSkyBlue);
+circle(160,160,40);
+FloodFill(160,160,clRed);
+circle(230,200,50);
+FloodFill(230,200,clFuchsia);
+circle(310,240,60);
+FloodFill(310,240,clBlack);
+circle(400,280,70);
+FloodFill(400,280,clPurple );
+circle(500,320,80);
+FloodFill(500,320,clOlive);
+END.
---
+++ лаб 11-12 паскаль/л 11-14 з4.pas
@@ -0,0 +1,13 @@
+﻿uses graphabc;
+begin
+setpencolor(clrandom);
+circle(100,50,100);
+circle(100,50,90);
+circle(100,50,80);
+circle(100,50,70);
+circle(100,50,60);
+circle(100,50,50);
+circle(100,50,40);
+circle(100,50,30);
+circle(100,50,20);
+end.
